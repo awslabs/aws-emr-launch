0a568d5e2a417ec90eacf573c89c23fd3ff6c0c7
Chauncy McCaughey <chamcca@amazon.com>
2020-01-30 13:49:48 -0700

    undo optional cluster_configuration

--- examples/emr_launch_function/app.py
+++ examples/emr_launch_function/app.py
@@ -34,8 +34,9 @@ cluster_config = cluster_configuration.ClusterConfiguration.from_stored_configur
 
 # Create a new State Machine to launch a cluster with the Basic configuration
 # Unless specifically indicated, fail to start if a cluster of the same name
-# is already running.
-launch_function_1 = emr_launch_function.EMRLaunchFunction(
+# is already running. Allow any parameter in the default override_interface to
+# be overwritten.
+launch_function = emr_launch_function.EMRLaunchFunction(
     stack, 'EMRLaunchFunction',
     launch_function_name='launch-basic-cluster',
     cluster_configuration=cluster_config,
@@ -46,17 +47,5 @@ launch_function_1 = emr_launch_function.EMRLaunchFunction(
     default_fail_if_cluster_running=True,
     allowed_cluster_config_overrides=cluster_config.override_interfaces['default'],
     cluster_tags=[core.Tag('Key1', 'Value1'), core.Tag('Key2', 'Value2')])
-
-# Create another State Machine to launch a Cluster with the sse-kms-profile but
-# allow the cluster_configuration to be provided at function execution time
-launch_function_2 = emr_launch_function.EMRLaunchFunction(
-    stack, 'EMRLaunchFunction2',
-    launch_function_name='launch-any-cluster',
-    emr_profile=sse_kms_profile,
-    cluster_name='dynamic-cluster',
-    default_fail_if_cluster_running=True,
-    allowed_cluster_config_overrides={
-        'ClusterName': 'Name'
-    })
 
 app.synth()
