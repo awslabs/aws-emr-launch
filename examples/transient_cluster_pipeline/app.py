2f2596a3244fd66643ad6bb7e92dc9d351b81ace
Chauncy McCaughey <chamcca@amazon.com>
2020-03-04 12:15:21 -0700

    updated transient example

--- examples/transient_cluster_pipeline/app.py
+++ examples/transient_cluster_pipeline/app.py
@@ -87,6 +87,23 @@ for i in range(5):
         cluster_id=sfn.TaskInput.from_data_at('$.LaunchClusterResult.ClusterId').value)
     phase_1.branch(step_task)
 
+# Define an example Validation Step
+emr_step = emr_code.EMRStep(
+    name='Validate Phase 1',
+    jar='s3://us-west-2.elasticmapreduce/libs/script-runner/script-runner.jar',
+    args=[
+        f'{step_code.s3_path}/phase_1/test_validation.sh'
+    ],
+    code=step_code
+)
+# Define an AddStep Task for the Validation Step
+validate_phase_1 = emr_tasks.AddStepBuilder.build(
+    stack, 'ValidatePhase1',
+    name='Validate Phase 1',
+    emr_step=emr_step,
+    cluster_id=sfn.TaskInput.from_data_at('$.LaunchClusterResult.ClusterId').value,
+    result_path='$.ValidatePhase1Result').add_catch(fail, errors=['States.ALL'], result_path='$.Error')
+
 
 # Create a Parallel Task for the Phase 2 Steps
 phase_2 = sfn.Parallel(stack, 'Phase2', result_path='$.Result.Phase2')
@@ -120,6 +137,28 @@ for i in range(5):
         cluster_id=sfn.TaskInput.from_data_at('$.LaunchClusterResult.ClusterId').value)
     phase_2.branch(step_task)
 
+# Define an example Validation Step
+emr_step = emr_code.EMRStep(
+    name='Validate Phase 2',
+    jar='command-runner.jar',
+    args=[
+        'hive-script',
+        '--run-hive-script',
+        '--args',
+        '-f',
+        f'{step_code.s3_path}/phase_2/test_validation.hql'
+    ],
+    code=step_code
+)
+# Define an AddStep Task for the Validation Step
+validate_phase_2 = emr_tasks.AddStepBuilder.build(
+    stack, 'ValidatePhase2',
+    name='Validate Phase 2',
+    emr_step=emr_step,
+    cluster_id=sfn.TaskInput.from_data_at('$.LaunchClusterResult.ClusterId').value,
+    result_path='$.ValidatePhase2Result').add_catch(fail, errors=['States.ALL'], result_path='$.Error')
+
+
 # Define a Task to Terminate the Cluster
 terminate_cluster = emr_tasks.TerminateClusterBuilder.build(
     stack, 'TerminateCluster',
@@ -138,7 +177,9 @@ success = emr_chains.Success(
 definition = sfn.Chain \
     .start(launch_cluster) \
     .next(phase_1) \
+    .next(validate_phase_1) \
     .next(phase_2) \
+    .next(validate_phase_2) \
     .next(terminate_cluster) \
     .next(success)
 
---
+++ examples/transient_cluster_pipeline/step_sources/phase_1/test_validation.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+
+echo "hello world" && echo "Args: $@" && sleep 30 && exit 0
